6ae4036f0da4d6d0424decffbb5a453e85aece18
madasilva1 <madasilva195@gmail.com>
2019-10-25 12:04:57 -0400

    Final commit

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.android.justjava;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.text.NumberFormat;
-
-//import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     int quantity = 2;
@@ -22,9 +17,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-
-        display(quantity);
-        displayPrice(quantity*5);
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\n" + "Thank you!";
+         displayMessage(priceMessage);
     }
 
     /**
@@ -50,6 +46,13 @@ public void Decrement(View view){
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
     /**
      * This method displays the given price on the screen.
